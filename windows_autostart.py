fd455177ae65250d2525fa4829e8de79bbb62f0d
Lakshay Jain <lakshaybusiness.12@gmail.com>
2025-09-10 08:47:45 +0530

    chore: corrected file name

--- windows_autostart.py
+++ windows_autostart.py
@@ -2,7 +2,7 @@ import os
 import shutil
 from pathlib import Path
 
-exe_path = Path("dist/autoOcr.exe").resolve()
+exe_path = Path("dist/auto_ocr.exe").resolve()
 startup_dir = Path(os.environ["APPDATA"]) / "Microsoft" / "Windows" / "Start Menu" / "Programs" / "Startup"
 
 shutil.copy(exe_path, startup_dir / exe_path.name)
